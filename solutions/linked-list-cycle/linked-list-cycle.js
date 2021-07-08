e5f0d5aec83a62ef2d4f709f01d2e5b1ff79f5a5
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2021-07-08 14:01:50 -0700

    Update linked-list-cycle.js

--- solutions/linked-list-cycle/linked-list-cycle.js
+++ solutions/linked-list-cycle/linked-list-cycle.js
@@ -10,14 +10,27 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+// Goal: See if given linked list is a circular linked list, meaning a last node will loop around to an earlier node
+
+// check if list is valid
+  // if not then return false
+
+// create reference to head for traversal
+// track index
+
+// traverse thru list
+  // check if index property exists on current node, will only exist if node was traversed
+    // if yes then return
+    // if no then assign index property
+  // update current head with next node
+// return false by default if cycle did not trigger
+
 var hasCycle = function(head, pos) {
   if (!head) {
     return false;
   }
-  // loop thru list
-    // if index property exists, cycle exists
-      // return true
-    // else assign index
+
   let node = head;
   let index = 0;
   
