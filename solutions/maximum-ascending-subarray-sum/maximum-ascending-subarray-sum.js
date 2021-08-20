d303ddea152ea0c8e93d53cae9299c11dc86ec94
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2021-08-20 12:32:39 -0700

    Create maximum-ascending-subarray-sum.js

---
+++ solutions/maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// check if nums is size 1
+    // if so return item in nums since it's the only subarray
+// store first item for base comparison
+// track max sum
+// track current sum
+// iterate thru array
+    // check if current item is greater than prev
+        // if yes then add current number to current sum
+            // update max sum with current sum and last max sum
+        // if no then reset current sum with current number
+    // update prev item with current item
+// return max sum
+
+var maxAscendingSum = function(nums) {
+    if (nums.length === 1) {
+        return nums[0];
+    }
+    
+    let prev = nums[0];
+    let maxSum = prev;
+    let currSum = prev;
+    
+    for (let i = 1; i < nums.length; i++) {
+        let curr = nums[i];
+        
+        if (curr > prev) {
+            currSum += curr;
+            maxSum = Math.max(maxSum, currSum);
+        } else {
+            currSum = curr;
+        }
+        
+        prev = curr;
+    }
+    
+    return maxSum;    
+};
+
+// Time complexity is O(N) as it only iterates through n items in nums array.
+// Space complexity is O(1) as only primitives were created.
