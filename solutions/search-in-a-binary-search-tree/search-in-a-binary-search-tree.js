641709d8d758b9d75468e1e3f4f4ea29ea870fd9
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2022-01-11 12:44:41 -0800

    Update search-in-a-binary-search-tree.js

--- solutions/search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
+++ solutions/search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -12,6 +12,8 @@
  * @return {TreeNode}
  */
 
+// Goal = Given a root of a tree, find out if tree has a node equal to val and return the node.
+
 // recursion
 // base: check if root doesn't exist
     // if yes then return null
@@ -21,19 +23,21 @@
     // if yes then recurse using left child of root, and val
     // if no then recurse using right child of root, and val
 
-
 var searchBST = function(root, val) {
-    if (root === null) {
-        return null;
+    if (!root) {
+        return null
     }
     
     if (root.val === val) {
-        return root;
-    }    
-
-    if (val < root.val) {
+        return root
+    }
+    
+    if (root.val > val) {
         return searchBST(root.left, val)
     } else {
         return searchBST(root.right, val)
     }
 };
+
+// Time complexity = O(LogN)
+// Space complexity = O(1)
