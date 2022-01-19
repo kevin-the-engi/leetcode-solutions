d943e5bff6982f64d1cb1e7c6505620387e5034c
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2022-01-19 12:13:06 -0800

    Update first-bad-version.py

--- solutions/first-bad-version/first-bad-version.py
+++ solutions/first-bad-version/first-bad-version.py
@@ -41,4 +41,31 @@ class Solution:
         
 # Time complexity = O(log n) since sample size is reduced by half each time.
 # Space complexity = O(1)
-        
+
+
+# ITERATIVE SOLUTION
+
+# Goal = Given a callback function, use function to find the first occurrence of true for n.
+# Use binary search with n as the end value and start at 0
+
+# loop while start is less than end
+#     get mid value from start and end
+#     if callback with mid value is false and callback with mid + 1 is true, then mid + 1 is the first bad version
+#     if callback for both mid and mid + 1 is false, then bad version is on right side so update start with mid
+#     else if they are both true, then bad version is on left so update n with mid
+
+class Solution:
+    def firstBadVersion(self, n: int, start = 0) -> int:
+        while (start <= n):
+            mid = math.floor((n - start) / 2) + start
+            
+            if not isBadVersion(mid) and isBadVersion(mid + 1):
+                return mid + 1
+            
+            if not isBadVersion(mid) and not isBadVersion(mid + 1):
+                start = mid
+            else:
+                n = mid
+
+# Time complexity = O(log n)
+# Space complexity = O(1)
