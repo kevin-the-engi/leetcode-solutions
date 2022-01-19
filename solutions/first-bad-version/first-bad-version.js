ceffce5652b04bece0192e4419b7eb56b19056d6
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2022-01-19 12:11:55 -0800

    Update first-bad-version.js

--- solutions/first-bad-version/first-bad-version.js
+++ solutions/first-bad-version/first-bad-version.js
@@ -53,3 +53,49 @@ var solution = function(isBadVersion) {
 
 // Time complexity = O(log n) since sample size is reduced by half each time.
 // Space complexity = O(1)
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+// ITERATIVE SOLUTION
+
+// Goal = Given a callback function, use function to find the first occurrence of true for n.
+// Use binary search with n as the end value and start at 0
+
+// loop while start is less than end
+    // get mid value from start and end
+    // if callback with mid value is false and callback with mid + 1 is true, then mid + 1 is the first bad version
+    // if callback for both mid and mid + 1 is false, then bad version is on right side so update start with mid
+    // else if they are both true, then bad version is on left so update n with mid
+
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n, start = 0) {
+        while (start <= n) {
+            let mid = Math.floor((n - start) / 2) + start
+        
+            if (!isBadVersion(mid) && isBadVersion(mid + 1)) {
+                return mid + 1
+            }
+
+            if (!isBadVersion(mid) && !isBadVersion(mid + 1)) {
+                start = mid
+            } else if (isBadVersion(mid) && isBadVersion(mid + 1)) {
+                n = mid
+            }
+        }
+    };
+};
+
+// Time complexity = O(log n)
+// Space complexity = O(1)
