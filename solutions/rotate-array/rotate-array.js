5cb2ab25cb677ccd1abcbf112865c838747a1e3b
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2022-01-24 12:43:39 -0800

    Update rotate-array.js

--- solutions/rotate-array/rotate-array.js
+++ solutions/rotate-array/rotate-array.js
@@ -3,32 +3,31 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate1 = function(nums, k) {
-    while (k > 0) {
-        let value = nums.pop();
-        nums.unshift(value);
-        k--;
-    }
-    
-    return nums;
-};
 
-// create a rotated version of the nums array by working by using i + k % length to get index to store first value and continue iterating
-// replace every element in nums array with rotated version
+// Goal = Given an array, move/rotate values by k places/steps.
+
+// create an array of size nums length
+// loop thru nums array
+    // get index for rotated array using index + k % nums length so end values loop around correctly
+    // place current value into new index of rotated array
+// loop thru nums array
+    // replace each value with those in rotated array
+// return nums
 
 var rotate = function(nums, k) {
-    let rotated = [];
+    let rotated = new Array(nums.length)
     
-    for(let i = 0; i < nums.length; i++){
-        rotated[(i + k) % nums.length] = nums[i];
+    for (let i = 0; i < nums.length; i++) {
+        let index = (i + k) % nums.length
+        rotated[index] = nums[i]
     }
     
-    for(let i = 0; i < nums.length; i++){
-        nums[i] = rotated[i];
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = rotated[i]
     }
     
-    return nums;
+    return nums
 };
 
-// Time complexity = O(N)
-// Space complexity = O(N)
+// Time complexity = O(n)
+// Space complexity = O(n) for dynamic array. However, O(1) for fixed-size array
