503c242f88a928f7f0a7a75452dd88378640236a
Kevin Ng <76230767+kevin-the-engi@users.noreply.github.com>
2022-06-29 16:20:55 -0700

    Update partitioning-into-minimum-number-of-deci-binary-numbers.py

--- solutions/partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ solutions/partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,18 +1,13 @@
-/**
- * @param {string} n
- * @return {number}
- */
+# Goal: Given a string n, return the minimum number of operations needed to sum up to n using only deci-binary numbers.
+# The output should be equal to the max digit in the string since all other numbers will be reduced to 0 by the time the max number reaches 0.
 
-// Goal: Given a string n, return the minimum number of operations needed to sum up to n using only deci-binary numbers.
-// The output should be equal to the max digit in the string since all other numbers will be reduced to 0 by the time the max number reaches 0.
+# split the string into an array
+# convert the strings into numbers
+# find max using resulting array
 
-// split the string into an array
-// convert the strings into numbers
-// find max using resulting array
-
-var minPartitions = function(n) {
-    return Math.max(...(n.split('').map(n => Number(n))));
-};
-
-// Time complexity = O(n)
-// Space complexity = O(n)
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return max(list(map(lambda x: int(x), list(n))))
+    
+# Time complexity = O(n)
+# Space complexity = O(n)
